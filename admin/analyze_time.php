e8c4c8e0fcd5258063d11b1c6189666e2cd2ba72
kwstasmel <33500174+kwstasmel@users.noreply.github.com>
2021-08-29 15:15:28 +0300

    code reformed

--- admin/analyze_time.php
+++ admin/analyze_time.php
@@ -3,10 +3,30 @@ define('__ROOT__', dirname(dirname(__FILE__)));
 require_once(__ROOT__.'/config.php');
 header('Content-Type: application/json');
 
-$sql="SELECT har FROM files";
+$result=[];
+$sql="SELECT avg(wait) as avgwait,HOUR(startedDateTime) as hour from harentries group by HOUR(startedDateTime)";
 $stmt=$conn->prepare($sql);
 $stmt->execute();
-$result=$stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+$res=$stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+$result[]=$res;
+
+$sql="SELECT weekday,avg(wait) as avgwait ,HOUR(startedDateTime) as hour FROM harentries GROUP BY HOUR(startedDateTime),weekday";
+$stmt=$conn->prepare($sql);
+$stmt->execute();
+$res=$stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+$result[]=$res;
+
+$sql="SELECT method,avg(wait) as avgwait, HOUR(startedDateTime) as hour FROM harentries GROUP BY method,HOUR(startedDateTime)";
+$stmt=$conn->prepare($sql);
+$stmt->execute();
+$res=$stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+$result[]=$res;
+
+$sql="SELECT uploader_provider,avg(wait) as avgwait, HOUR(startedDateTime) as hour FROM harentries GROUP BY uploader_provider,HOUR(startedDateTime)";
+$stmt=$conn->prepare($sql);
+$stmt->execute();
+$res=$stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+$result[]=$res;
 
 echo json_encode($result);
 ?>
