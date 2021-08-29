ccab973169a9be50f3e2ee88d62415827a20d1f4
kwstasmel <33500174+kwstasmel@users.noreply.github.com>
2021-08-30 00:33:57 +0300

    mysqli with pdo

--- admin/Login_Admin.php
+++ admin/Login_Admin.php
@@ -7,37 +7,27 @@ $password= $_POST['password'];
   
 
 $sql = "SELECT  username,password FROM user WHERE username = ? AND isAdmin=1";
-if($stmt_user = mysqli_prepare($conn,$sql)){
-   mysqli_stmt_bind_param($stmt_user,"s",$param_username);
-   $param_username = $username;
-   if(mysqli_stmt_execute($stmt_user)){
-   mysqli_stmt_store_result($stmt_user);
-    if(mysqli_stmt_num_rows($stmt_user)== 1){
-      mysqli_stmt_bind_result($stmt_user,$username,$hashed_password);
-       if(mysqli_stmt_fetch($stmt_user)){
-         if(password_verify($password,$hashed_password)){
-          session_start();
-          // Store data in session variables
-            $_SESSION["admin_loggedin"] = true;
-            $_SESSION["admin_username"] = $username;                             
-            echo"success";
-            exit;
-          }
-          else{
-            echo"wrong_password"; 
-            exit;                
-          }                
-        }            
-      }      
-     else{
-      echo"wrong_username"; 
-      exit;          
-     }     
-  }    
-  mysqli_stmt_close($stmt_user);
-}    
-
-mysqli_close($conn);
+$stmt=$conn->prepare($sql);
+$stmt->bind_param("s",$username);
+$stmt->execute();
+$res=$stmt->get_result()->fetch_assoc();
+if(!empty($res["username"])){
+  if(password_verify($password,$res["password"])){
+    session_start();
+    $_SESSION["admin_loggedin"] = true;
+    $_SESSION["admin_username"] = $username;
+    echo "success";
+    exit;
+  }
+  else{
+    echo"wrong_password";
+    exit;
+  }
+}
+else{
+  echo"wrong_username";
+  exit;
+}
 
 ?>
 
--- user/Register_User.php
+++ user/Register_User.php
@@ -2,25 +2,24 @@
 define('__ROOT__', dirname(dirname(__FILE__)));
 require_once(__ROOT__.'/config.php');
 
+
 $username= $_POST['username'];
 $password= $_POST['password'];
 $email=    $_POST['email'];
 
-$sql = "INSERT INTO user (username, password,email,isAdmin) VALUES (?, ?, ?,False)";
-if($stmt = mysqli_prepare($conn, $sql)){
- // κάνει bind τις τιμές στο statement
- $param_username=$username;
- $param_password=password_hash($password, PASSWORD_DEFAULT);
- $param_email=$email;
- mysqli_stmt_bind_param($stmt, 'sss', $param_username,$param_password,$param_email);
- if(mysqli_stmt_execute($stmt)){
-   echo"success";
-	}
-	else{
-	echo"error";
-	}
-	mysqli_stmt_close($stmt);
-}
-mysqli_close($conn);
+$param_password=password_hash($password, PASSWORD_DEFAULT);
 
+$sql = "INSERT INTO user (username, password,email,isAdmin) VALUES (?, ?, ?,False)";
+$stmt=$conn->prepare($sql);
+$stmt->bind_param("sss",$username,$param_password,$email);
+if($stmt->execute()){
+	echo"success";
+	$stmt->close();
+	exit;
+}
+else{
+	echo"error";
+	$stmt->close();
+	exit;
+}
 ?>
