45cd811848adfe682b3b3876819edd24917fa00e
kwstasmel <33500174+kwstasmel@users.noreply.github.com>
2021-09-01 01:19:46 +0300

    1/9/2021

--- admin/Admin_Visual.js
+++ admin/Admin_Visual.js
@@ -5,9 +5,32 @@ $(function(){
     dataType:"json"
   });
   request.done(function(response,TextStatus,jqXHR){
-
+    console.log(response);
+    var users=$.map(response[0],function(o){return {user:o.username, location:o.uploader_lat_long, city:o.uploader_city}});
+    console.log(users);
+    maxiter=response[0].length;
+    for(i=0; i<maxiter; i++){
+      let user_loc=response[0][i].loc.split(",");
+      let uploader_loc=response[0][i].uploader_lat_long.split(",");
+      let user_loc_array=[parseFloat(user_loc[0]),parseFloat(user_loc[1])];
+      let uploader_loc_array=[parseFloat(uploader_loc[0]),parseFloat(uploader_loc[1])];
+      var usermarker = new L.Marker(user_loc_array).addTo(map);
+      var polyline = new L.polyline([uploader_loc_array,user_loc_array]);
+    }
+      var loc_array=$.map(response[0],function(o){
+        let temp_user=o.loc.split(",");
+        let temp_uploader=o.uploader_lat_long.split(",");
+        let temp_array=[parseFloat(temp_user[0]),parseFloat(temp_user[1])];
+        let temp_array_uploader=[parseFloat(temp_uploader[0]),parseFloat(temp_uploader[1])];
+        var polyline= new L.polyline([temp_array_uploader,temp_array]
+          //,{weight:(o.count/80)}
+          ).addTo(map)
+        var marker =new L.Marker(temp_array).addTo(map);
+        var marker_uploader = new L.marker(temp_array_uploader).addTo(map).bindPopup(o.username +" from "+ o.uploader_city);
+        });
+        
+    
   })
-
   var baseLayer=L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -16,22 +39,12 @@ $(function(){
     zoomOffset: -1,
     accessToken: 'pk.eyJ1Ijoia3dzdGFzbWVsIiwiYSI6ImNrcnoydGhoNDBuamUybm85MmhnaHJlcTQifQ.mFbkt4WVCUKo3hLpKbZqOA'
   });
-  var cfg = {
-  "radius": 20,
-  "maxOpacity": 0.8,
-  "scaleRadius":false,
-  "useLocalExtrema": false,
-  valueField: 'count',
-  latField: 'lat',
-  lngField: 'lng',
-  };
-  let heatmapLayer= new HeatmapOverlay(cfg);
 
   var map=new L.Map('mapid',{
     center: new L.LatLng(38.246639, 21.734573),
     zoom:4,
-    layers:[baseLayer,heatmapLayer]
+    layers:[baseLayer]
   });
 
-  heatmapLayer.addData(response);
+
 })
--- admin/Admin_visual.php
+++ admin/Admin_visual.php
@@ -1,7 +1,21 @@
-<?
+<?php
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 define('__ROOT__', dirname(dirname(__FILE__)));
 require_once(__ROOT__.'/config.php');
 header('Content-Type: application/json');
-session_start();
+
+
+$result=[];
+$sql="SELECT username,uploader_city,uploader_lat_long,server_lat_long as loc,COUNT(server_lat_long) as count FROM harentries INNER JOIN user on uploader=User_id  GROUP BY server_lat_long, uploader ";
+$stmt= $conn->prepare($sql);
+$stmt->execute();
+$res=$stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+$result[]=$res;
+
+$sql="SELECT";
+$stmt= $conn->prepare($sql);
+$stmt->execute();
+$res=$stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+$result[]=$res;
+echo json_encode($result);
 ?>
