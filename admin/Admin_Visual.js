2e8ae06397882531d59cd6278d887b3847acdd5a
kwstasmel <33500174+kwstasmel@users.noreply.github.com>
2021-09-01 03:52:51 +0300

    fixed 1/9/21

--- admin/Admin_Visual.js
+++ admin/Admin_Visual.js
@@ -4,31 +4,48 @@ $(function(){
     url:"Admin_visual.php",
     dataType:"json"
   });
+  var redIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
   request.done(function(response,TextStatus,jqXHR){
     console.log(response);
-    var users=$.map(response[0],function(o){return {user:o.username, location:o.uploader_lat_long, city:o.uploader_city}});
-    console.log(users);
+    
+    // marker στις τοποθεσίες που έχουν γίνει http αιτήσεις
     maxiter=response[0].length;
     for(i=0; i<maxiter; i++){
-      let user_loc=response[0][i].loc.split(",");
-      let uploader_loc=response[0][i].uploader_lat_long.split(",");
-      let user_loc_array=[parseFloat(user_loc[0]),parseFloat(user_loc[1])];
-      let uploader_loc_array=[parseFloat(uploader_loc[0]),parseFloat(uploader_loc[1])];
-      var usermarker = new L.Marker(user_loc_array).addTo(map);
-      var polyline = new L.polyline([uploader_loc_array,user_loc_array]);
+      let loc=response[0][i].loc.split(",");
+      let loc_array=[parseFloat(loc[0]),parseFloat(loc[1])];
+      var marker = new L.Marker(loc_array).addTo(map);
     }
-      var loc_array=$.map(response[0],function(o){
-        let temp_user=o.loc.split(",");
-        let temp_uploader=o.uploader_lat_long.split(",");
-        let temp_array=[parseFloat(temp_user[0]),parseFloat(temp_user[1])];
-        let temp_array_uploader=[parseFloat(temp_uploader[0]),parseFloat(temp_uploader[1])];
-        var polyline= new L.polyline([temp_array_uploader,temp_array]
-          //,{weight:(o.count/80)}
-          ).addTo(map)
-        var marker =new L.Marker(temp_array).addTo(map);
-        var marker_uploader = new L.marker(temp_array_uploader).addTo(map).bindPopup(o.username +" from "+ o.uploader_city);
-        });
-        
+    // marker στις τοποθεσίες των uploader
+    maxiter_uploader=response[1].length;
+    
+    for(i=0; i<maxiter_uploader; i++){
+      let loc=response[1][i].uploader_lat_long.split(",");
+      let loc_array=[parseFloat(loc[0]),parseFloat(loc[1])];
+      var marker_uploader= new L.Marker(loc_array, {icon: redIcon}).addTo(map).bindPopup("Uploaders : "+response[1][i].numberofuploaders).openPopup();
+    }
+    // ανάκτησε το μέγιστο πλήθος request που έχει δεχτεί ένα ip 
+    var max_count=response[3][0].maximum;
+
+    // draw polylines για κάθε request του uploader σε κάθε ip(marker)
+    maxiter_server=response[2].length;
+    for(i=0; i<maxiter_server; i++){
+      let loc_server=response[2][i].loc.split(",");
+      let loc_array_ser=[parseFloat(loc_server[0]),parseFloat(loc_server[1])];
+      let loc_uploader=response[2][i].uploader_lat_long.split(",");
+      let loc_array_up=[parseFloat(loc_uploader[0]),parseFloat(loc_uploader[1])];
+      let temp_count=response[2][i].count;
+      var polyline= new L.polyline([loc_array_ser,loc_array_up],{weight:8*(temp_count/max_count)}).addTo(map)
+    }
+   
+  
+
     
   })
   var baseLayer=L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
--- admin/Admin_visual.php
+++ admin/Admin_visual.php
@@ -6,16 +6,29 @@ header('Content-Type: application/json');
 
 
 $result=[];
-$sql="SELECT username,uploader_city,uploader_lat_long,server_lat_long as loc,COUNT(server_lat_long) as count FROM harentries INNER JOIN user on uploader=User_id  GROUP BY server_lat_long, uploader ";
+$sql="SELECT DISTINCT(server_lat_long) as loc FROM harentries ";
 $stmt= $conn->prepare($sql);
 $stmt->execute();
 $res=$stmt->get_result()->fetch_all(MYSQLI_ASSOC);
 $result[]=$res;
 
-$sql="SELECT";
+$sql="SELECT uploader_lat_long, COUNT(DISTINCT(uploader)) as numberofuploaders FROM harentries GROUP BY uploader_lat_long;";
 $stmt= $conn->prepare($sql);
 $stmt->execute();
 $res=$stmt->get_result()->fetch_all(MYSQLI_ASSOC);
 $result[]=$res;
+
+$sql="SELECT uploader_lat_long,server_lat_long as loc ,COUNT(server_lat_long) as count FROM harentries GROUP BY server_lat_long,uploader";
+$stmt= $conn->prepare($sql);
+$stmt->execute();
+$res=$stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+$result[]=$res;
+
+$sql="SELECT MAX(count) as maximum FROM(SELECT server_lat_long as loc,COUNT(server_lat_long) as count FROM harentries  GROUP BY server_lat_long) AS T" ;
+$stmt= $conn->prepare($sql);
+$stmt->execute();
+$res=$stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+$result[]=$res;
+
 echo json_encode($result);
 ?>
